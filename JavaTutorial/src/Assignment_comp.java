f3993222ad5af1c4aa4ccc0ccf251d345f11e8be
HP <HP@AssassinCORE>
2021-01-11 21:37:49 +0530

    First commit

---
+++ JavaTutorial/src/Assignment_comp.java
@@ -0,0 +1,20 @@
+
+public class Assignment_comp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 //Difference of two numbers
+		byte a = 5;
+		byte b = 3;
+		byte Difference = (byte) (a - b);
+		System.out.println("The difference of 5 and 3 is: "+Difference);
+		
+		//Product of three numbers
+		byte x = 5;
+		byte y = 6;
+		byte z = 7;
+		short Product = (short) (x*y*z);
+		System.out.print("The product of 5,6 and 7 is: "+Product);
+	}
+
+}
